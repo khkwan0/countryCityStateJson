47737e759aa7211c5fce02e50e76d31a5bf26847
Kenneth Kwan <khkwan0@gmail.com>
2019-02-27 03:06:43 +0000

    added original sources to allow for editing

---
+++ src/country-state-city/index.js
@@ -0,0 +1,44 @@
+var countryList = require("./lib/country.json")
+var stateList = require("./lib/state.json")
+var cityList = require("./lib/city.json")
+
+var country_state_city = {
+	getCountryById: function(id) {
+		if(!isNaN(+(id)))
+			return countryList[+(id)]
+		else
+			return ""
+	},
+	getStateById: function(id) {
+		if(!isNaN(+(id)))
+			return stateList[+(id)];
+		else
+			return ""
+	},
+	getCityById: function(id) {
+		if(!isNaN(+(id)))
+			return cityList[+(id)];
+		else
+			return ""
+	},
+	getStatesOfCountry: function(countryId) {
+		var states = stateList.filter(function(value, index) {
+			return value.country_id == countryId
+		})
+		return states
+	},
+	getCitiesOfState: function(stateId) {
+    console.log(stateId)
+		var cities = cityList.filter(function(value, index) {
+//      console.log(value, index)
+			return value.state_id == stateId
+		})
+		return cities
+	},
+	getAllCountries: function() {
+		return countryList;
+	}
+
+}
+
+module.exports = country_state_city;
